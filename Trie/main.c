05bd7d0fbdd5bd9991cfab3776701a5f060acd0a
Carlos <camutze@gamil.com>
2023-11-19 20:39:32 -0300

    comentario

--- Trie/main.c
+++ Trie/main.c
@@ -7,7 +7,6 @@
 
 int main(int argc, char **argv)
 {
-    arvore_trie *arvore;
     FILE *base, *arq;
 
     setlocale(LC_CTYPE, "");
@@ -26,29 +25,20 @@ int main(int argc, char **argv)
     if (!(arq = fopen(argv[2], "r")))
         tratar_erro("ERRO_AO_ABRIR_ARQUIVO_ARQ");
 
-    arvore = criar_arvore(argv[1], base);
 
     while (!feof(arq))
     {
         char palavra[100];
         fscanf(arq, "%s", palavra);
-
+        
         word_pontua(palavra);
         if (word_acento(palavra) && strlen(palavra) >= 4)
         {
             word_sem_minusc(palavra);
             printf("%s\n", palavra);
-            trie_inserir(arvore->raiz, palavra);
-
             fprintf(base, "%s %s\n", palavra, argv[2]);
             
         }
     }
-    fclose(arq);
-    fclose(base);
-
-    trie_imprime(arvore->raiz);
-    trie_destruir_nodo(arvore->raiz);
-    free(arvore->arquivos);
-    free(arvore);
+   
 }
--- Trie/trie_t.h
+++ Trie/trie_t.h
@@ -5,49 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAMANHO_ALFABETO 26
-#define N_DE_ARQUIVOS 14 //numero maximo de arquivos que podem ser lidos
-
-typedef struct tree_trie
-{
-    struct trie_node *raiz;
-    char **arquivos;
-} arvore_trie;
-
-typedef struct trie_node
-{
-    struct trie_node *filhos[TAMANHO_ALFABETO];
-    unsigned short *id_arquivo; //vetorque armazena e associa o id do arquivo com a palavra
-    int is_end;
-} trie_node;
-
-/* Finaliza execucao e imprime a mensagem
- que foi passada como parametro*/
 void tratar_erro(const char *msg);
-
-arvore_trie *criar_arvore(char *arquivo, FILE *base);
-
-/* Cria um novo no da trie e retorna o ponteiro para ele */
-trie_node *criar_no(void);
-
-/* Insere uma nova chave na trie */
-void trie_inserir(trie_node *raiz, const char *chave);
-
-/* Busca uma chave na trie */
-int trie_busca(trie_node *raiz, const char *chave);
-
-
-void trie_imprime_prefixo(trie_node *raiz, const char *prefixo);
-
-void trie_imprime(trie_node *raiz);
-
-void trie_destruir_nodo(trie_node *raiz);
 
 int word_acento(char *str);
 
 void word_pontua(char *str);
 
 void word_sem_minusc(char *str);
+
+
 
 
 #endif
